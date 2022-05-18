435796344f6b17f1322c6156d0fd732d451d4058
olga-zeinalova <104780375+olga-zeinalova@users.noreply.github.com>
2022-05-18 17:55:22 +0200

    initial commit

---
+++ src/main/java/classroom/Main.java
@@ -0,0 +1,101 @@
+package classroom;
+
+public class Main {
+
+    int timeout = 10;
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello Olga JAVA 1 Lekcija");
+
+
+        //EN:Integer - LV:Vesels skaitlis
+        int x = 5;
+        int y = 10;
+
+
+        System.out.println("x ir vienāds ar :" );
+        System.out.println(x);
+        System.out.println("y ir vienāds ar :" );
+        System.out.println(y);
+        System.out.println("x+y ir vienāds ar :" );
+        System.out.println("x+y" );
+
+        //[Mainīga tips] [Mainīgā nosaukums] = [Vērtība];
+//Vārdi un teikumi JAVA valodā tiek saglabāti mainīgā tipā Štring'
+        String dogName = "Reksis";
+        //Veseli skaitļi tiek definēti ar mainīga tipu īnt'
+        int dogAge = 10;
+        //Daiļskaitļi tiek definēti ar mainīga tipu ''double'vai ''float'
+        double dogWeight = 15.5;
+        String dogBreed = "VAS";
+        // Jā(true) Nē(false) jautājums tiek definēts ar mainīgā tipu ''oolean'
+        boolean IsDogHungry = true;
+
+        //Vienu simbolu definē ar mainīgā tipu čhar'
+
+        char dogGender = 'M';
+
+        char currency ='$';
+        int dogPrice = 100;
+
+
+        System.out.println("Suņa vārds: " + dogName);
+        System.out.println("Suņa Vecums: " + dogAge );
+        System.out.println("Suņa Svars: " + dogWeight);
+        System.out.println("Suņa Šķirna: " + dogBreed);
+        System.out.println("Vai suns ir izsalcis?: "  + IsDogHungry);
+        System.out.println("Suņa dzimums ir: " + dogGender);
+        System.out.println("Suņa kucēni maksā : " + dogPrice + currency );
+
+
+byte age = 10;
+short deepestSeaLevel = 11000;
+int highestIntNumber = 2147483647;
+long USACountryDep = 2147483647232L;
+        System.out.println(age);
+        System.out.println(highestIntNumber);
+        System.out.println(USACountryDep);
+
+
+        double lowestTempoerature = 20.4;
+
+        float highestTemperature = 40.64537829F;
+
+        System.out.println("Augstākā temperatūra ir " + highestTemperature + "Zemāka temp. ir" +lowestTempoerature);
+
+
+
+
+
+                int summa;
+        int pirmaisSkaitlis = 20;
+        int otraisSkaitlis = -9;
+        summa = pirmaisSkaitlis + otraisSkaitlis;
+        System.out.println(pirmaisSkaitlis +"+" + otraisSkaitlis + "=" + summa);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
---
+++ src/main/java/homework/FirstClass.java
@@ -0,0 +1,10 @@
+package homework;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("This is homework");
+
+
+    }
+}
